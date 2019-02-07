6475c6b0b43857dfe6e1a3dfed74a9c12d946cc2
joGordtt24 <jogordtt24@hanovernorwichschools.org>
2019-02-07 19:23:44 +0000

    List Comprehension

--- doubleMe.hs
+++ doubleMe.hs
@@ -1,3 +1,8 @@
-doubleMe x = x + x
 
-doubleUs x y = x*2 + y*2 
+fizzBuzz n f b = [if x `mod`(f*b) == 0 then "FizzBuzz"
+  else 
+    if x `mod` f == 0 then "Fizz"
+      else
+        if x `mod` b == 0 then "Buzz"
+          else show x
+            | x <- [1..n]] 
